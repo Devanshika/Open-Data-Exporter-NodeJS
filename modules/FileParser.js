dcec90ba4d2c3f7cb45b10da15b35464be8bc4dd
devanshika <devanshika4@gmail.com>
2021-01-30 17:57:11 -0500

    Contains a module that creates a json file for the datasets obtained from open data

---
+++ modules/FileParser.js
@@ -0,0 +1,23 @@
+///-------------------------------------------------------------------------------------------------
+// File: FileParser.js
+//
+// Author: Devanshika Ghosh
+// Date: 12/8/2020
+//
+// Summary:	This file creates the file that stores our datasets
+///-------------------------------------------------------------------------------------------------
+
+const { ModuleResolutionKind } = require("typescript");
+const { defaultCipherList } = require("constants");
+
+//constructor that stores file path
+function FileParser(file_settings){
+    this.exportPath=file_settings;
+    this.fs = require("fs");
+}
+FileParser.prototype.createFile= async function(datafile, fileName){
+    var datajson = JSON.stringify(datafile);
+    var fd = this.fs.openSync(this.exportPath+"//"+fileName,'w');
+    this.fs.writeFileSync(fd,datajson,"utf8");
+}
+module.exports=FileParser;
